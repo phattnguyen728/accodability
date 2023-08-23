fb78754582b65c4026597823da85be7c274cd9d6
Hung Hoang <hungduyhoang@gmail.com>
2023-08-23 21:59:34 +0000

    Update main.py Updated this file and added back CORSMiddleware components. -Hung

--- api/main.py
+++ api/main.py
@@ -7,6 +7,15 @@ import os
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:3000")],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(authenticator.router)
 app.include_router(users.router)
 app.include_router(posts.router)
