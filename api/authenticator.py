50944cfca7b6cd1d0c55555628eb73caa03285ac
Hung Hoang <hungduyhoang@gmail.com>
2023-08-23 21:56:12 +0000

    Update authenticator.py changed account to user

--- api/authenticator.py
+++ api/authenticator.py
@@ -1,34 +1,30 @@
 import os
 from fastapi import Depends
 from jwtdown_fastapi.authentication import Authenticator
-from queries.accounts import AccountQueries, AccountOut, AccountOutWithPassword
+from queries.users import UserOutWithPassword, UserQueries, UserOut
+
 
 class MyAuthenticator(Authenticator):
     async def get_account_data(
         self,
-        email: str,
-        accounts: AccountQueries,
+        username: str,
+        users: UserQueries,
     ):
-        # Use your repo to get the account based on the
-        # username (which could be an email)
-        return accounts.get(email)
+        return users.get_user(username)
 
     def get_account_getter(
         self,
-        accounts: AccountQueries = Depends(),
+        users: UserQueries = Depends(),
     ):
-        # Return the accounts. That's it.
-        return accounts
+        return users
 
-    def get_hashed_password(self, account: AccountOutWithPassword):
-        # Return the encrypted password value from your
-        # account object
-        return account.hashed_password
+    def get_hashed_password(self, user: UserOutWithPassword):
+        return user.hashed_password
 
-    def get_account_data_for_cookie(self, account: AccountOut):
-        # Return the username and the data for the cookie.
-        # You must return TWO values from this method.
-        return account.email, AccountOut(**account.dict())
+    def get_account_data_for_cookie(self, user: UserOut):
+        return user.id, UserOut(**user.dict())
 
 
-authenticator = MyAuthenticator(os.environ["SIGNING_KEY"])
+authenticator = MyAuthenticator(
+    os.environ["SIGNING_KEY"],
+)
