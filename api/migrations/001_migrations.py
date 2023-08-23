a6aa65f0db5d20f88b9e594c9ae7b1bfaeef7048
Diana <dianaaako@gmail.com>
2023-08-23 14:45:34 -0700

    updated table for posts

--- api/migrations/001_migrations.py
+++ api/migrations/001_migrations.py
@@ -9,7 +9,7 @@ steps = [
           avatar TEXT NOT NULL,
           email TEXT NOT NULL UNIQUE,
           username TEXT NOT NULL UNIQUE,
-          referrer_id INTEGER REFERENCES users("id") ON DELETE CASCADE
+          referrer_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
         """,
         # "Down" SQL statement
@@ -17,4 +17,19 @@ steps = [
         DROP TABLE users;
         """,
     ],
+    [
+        """
+        Create table post (
+            id serial primary key not null,
+            title varchar(150) not null,
+            body text not null,
+            hyperlink varchar(400),
+            author_id INT NOT NULL REFERENCES users(id),
+            created_at timestamp not null default current_timestamp
+        );
+        """,
+        """
+        DROP table post;
+        """,
+    ]
 ]
