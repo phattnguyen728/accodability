d4121539419cec19f92d07b318b7242073809dc8
Adam Kmak <adamlkmakdevelop@gmail.com>
2023-08-29 00:35:27 +0000

    #14 Remaining Protected Endpoints - Comments

--- api/queries/comments.py
+++ api/queries/comments.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from queries.pool import pool
-from typing import List, Literal, Union
+from typing import List, Union
 from pydantic import BaseModel
 
 
@@ -8,9 +8,12 @@ class Error(BaseModel):
     message: str
 
 
+class CommentEdit(BaseModel):
+    body: str
+
+
 class CommentIn(BaseModel):
     author_id: int
-    post_id: int
     body: str
 
 
@@ -20,10 +23,6 @@ class CommentOut(BaseModel):
     post_id: int
     body: str
     created_at: datetime
-
-
-class CommentListOut(BaseModel):
-    comments: list[CommentOut]
 
 
 class CommentQueries:
@@ -40,7 +39,6 @@ class CommentQueries:
                         """,
                         [post_id],
                     )
-                    print("test")
                     return [
                         CommentOut(
                             id=record[0],
@@ -52,5 +50,120 @@ class CommentQueries:
                         for record in cur
                     ]
         except Exception as e:
-            print(e)
-            return {"message": "Could not retrieve comments"}
+            return Error(
+                {"message": f"Could not retrieve comments due to: {e}"}
+            )
+
+    def create_comment(
+        self, post_id: int, comment: CommentIn
+    ) -> Union[CommentOut, Error]:
+        try:
+            ValueError
+            with pool.connection() as conn:
+                with conn.cursor() as cur:
+                    result = cur.execute(
+                        """
+                        INSERT INTO comments
+                            (author_id, post_id, body)
+                        VALUES
+                            (%s, %s, %s)
+                        RETURNING id, created_at;
+                        """,
+                        [comment.author_id, post_id, comment.body],
+                    )
+                    data = result.fetchone()
+                    id = data[0]
+                    created_at = data[1]
+                    old_data = comment.dict()
+                    return CommentOut(
+                        id=id,
+                        post_id=post_id,
+                        **old_data,
+                        created_at=created_at,
+                    )
+        except Exception as e:
+            return Error(
+                {"message": f"Could not create a comment due to: {e}"}
+            )
+
+    def delete_comment(self, post_id: int, id: int) -> bool:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        """
+                        DELETE FROM comments
+                        WHERE post_id = %s AND id = %s
+                        """,
+                        [post_id, id],
+                    )
+                    return True
+        except Exception as e:
+            return Error(
+                {"message": f"Could not delete specified comment due to: {e}"}
+            )
+
+    def update_comment(
+        self, post_id: int, id: int, body: CommentEdit
+    ) -> Union[CommentOut, Error]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        """
+                        UPDATE comments
+                        SET body = %s, created_at = %s
+                        WHERE post_id = %s AND id = %s
+                        """,
+                        [body.body, datetime.now(), post_id, id],
+                    )
+                    result = cur.execute(
+                        """
+                        SELECT * FROM comments
+                        WHERE id = %s
+                        """,
+                        [id],
+                    )
+                    data = result.fetchone()
+                    print(data)
+                    author_id = data[1]
+                    created_at = data[4]
+                    return CommentOut(
+                        id=id,
+                        author_id=author_id,
+                        post_id=post_id,
+                        body=body.body,
+                        created_at=created_at,
+                    )
+        except Exception as e:
+            return Error(
+                {"message": f"Could not delete specified comment due to: {e}"}
+            )
+
+    def get_comments_by_user(self, author_id: int) -> Union[CommentOut, Error]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        """
+                        SELECT id, author_id, post_id, body, created_at
+                        FROM comments
+                        WHERE author_id = %s
+                        ORDER BY created_at DESC
+                        """,
+                        [author_id],
+                    )
+                    return [
+                        CommentOut(
+                            id=record[0],
+                            author_id=record[1],
+                            post_id=record[2],
+                            body=record[3],
+                            created_at=record[4],
+                        )
+                        for record in cur
+                    ]
+        except Exception as e:
+            return Error(
+                {"message": f"Could not retrieve comments due to: {e}"}
+            )
--- api/routers/comments.py
+++ api/routers/comments.py
@@ -1,6 +1,12 @@
-from fastapi import APIRouter, Depends, HTTPException
-from typing import Optional, Union, List
-from queries.comments import Error, CommentQueries, CommentIn, CommentOut
+from fastapi import APIRouter, Depends
+from typing import Union, List
+from queries.comments import (
+    Error,
+    CommentQueries,
+    CommentIn,
+    CommentOut,
+    CommentEdit,
+)
 from psycopg.errors import ForeignKeyViolation
 from authenticator import authenticator
 
@@ -17,3 +23,52 @@ async def get_comments(
     account_data: dict = Depends(authenticator.get_current_account_data),
 ):
     return repo.get_comments(post_id)
+
+
+@router.post(
+    "/api/posts/{post_id}/comments",
+    response_model=Union[CommentOut, Error],
+)
+async def create_comment(
+    post_id: int,
+    comment: CommentIn,
+    repo: CommentQueries = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+):
+    return repo.create_comment(post_id, comment)
+
+
+@router.delete("/api/posts/{post_id}/comments/{id}", response_model=bool)
+async def delete_comment(
+    post_id: int,
+    id: int,
+    repo: CommentQueries = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+):
+    return repo.delete_comment(post_id, id)
+
+
+@router.put(
+    "/api/posts/{post_id}/comments/{id}",
+    response_model=Union[CommentOut, Error],
+)
+async def update_comment(
+    post_id: int,
+    id: int,
+    body: CommentEdit,
+    repo: CommentQueries = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+):
+    return repo.update_comment(post_id, id, body)
+
+
+@router.get(
+    "/api/users/{author_id}/comments/",
+    response_model=Union[List[CommentOut], Error],
+)
+async def get_comments_by_user(
+    author_id: int,
+    repo: CommentQueries = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+):
+    return repo.get_comments_by_user(author_id)
