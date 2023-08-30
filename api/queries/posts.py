aebd892838715ea5c9fceb1afe1fcb786975bdfb
Diana <dianaaako@gmail.com>
2023-08-30 23:07:49 +0000

    Postendpointforposts #13 #28

--- api/queries/posts.py
+++ api/queries/posts.py
@@ -12,7 +12,6 @@ class PostIn(BaseModel):
     title: str
     body: str
     hyperlink: Optional[str]
-    author_id: int
 
 
 class PostOut(BaseModel):
@@ -25,7 +24,7 @@ class PostOut(BaseModel):
 
 
 class PostListOut(BaseModel):
-    posts: list[PostOut]
+    posts: List[PostOut]
 
 
 class PostQueries:
@@ -48,3 +47,117 @@ class PostQueries:
         except Exception as e:
             print(e)
             return {"message": "Could not retrieve posts."}
+
+    def create_post(self, post_data: PostIn, author_id: int) -> PostOut:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as cur:
+                    result = cur.execute(
+                        """
+                        INSERT INTO posts (title, body, hyperlink, author_id, created_at)
+                        VALUES (%s, %s, %s, %s, %s)
+                        RETURNING id, author_id, created_at
+                        """,
+                        (
+                            post_data.title,
+                            post_data.body,
+                            post_data.hyperlink,
+                            author_id,
+                            datetime.now(),
+                         ),
+                    )
+                    data = result.fetchone()
+                    new_post_id = data[0]
+                    created_at = data[1]
+                    return PostOut(
+                        id=new_post_id,
+                        title=post_data.title,
+                        body=post_data.body,
+                        hyperlink=post_data.hyperlink,
+                        author_id=author_id,
+                        created_at=created_at,
+                    )
+        except Exception as e:
+            print(e)
+            return {"message": "Could not create post"}
+
+    # Get request to fetch specific post and load all comments
+    def get_post_by_id(self, post_id: int) -> Optional[PostOut]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        "SELECT * FROM posts WHERE id = %s",
+                        [post_id],
+                    )
+                    record = cur.fetchone()
+                    if record:
+                        return PostOut(
+                            id=record[0],
+                            title=record[1],
+                            body=record[2],
+                            hyperlink=record[3],
+                            author_id=record[4],
+                            created_at=record[5],
+                        )
+                    return None
+        except Exception as e:
+            print(e)
+
+    def delete_post(self, post_id: int, author_id: int) -> bool:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        """DELETE FROM posts WHERE id = %s AND author_id = %s""",
+                        [post_id, author_id],
+                    )
+                    if cur.rowcount == 1:
+                        cur.execute(
+                            """
+                            DELETE FROM comments
+                            WHERE post_id = %s
+                            """,
+                            [post_id],
+                        )
+                        return True
+                    else:
+                        return False
+        except Exception as e:
+            print(e)
+            return False
+    # put request still working on this code
+    # def update_post(self, post_id: int, post_data: PostIn) -> Optional[PostOut]:
+    #     try:
+    #         with pool.connection() as conn:
+    #             with conn.cursor() as cur:
+    #                 cur.execute(
+    #                     """
+    #                     UPDATE posts
+    #                     SET title = %s, body = %s, hyperlink = %s
+    #                     WHERE id = %s
+    #                     RETURNING id, created_at
+    #                     """,
+    #                     [post_data.title, post_data.body, post_data.hyperlink, post_id],
+    #                 )
+    #                 result = cur.execute(
+    #                     """
+    #                     SELECT * FROM posts
+    #                     WHERE id = %s
+    #                     """,
+    #                     [post_id],
+    #                 )
+    #                 record = result.fetchone()
+    #                 created_at = record[5]
+    #                 return PostOut(
+    #                     id=post_id,
+    #                     title=post_data.title,
+    #                     body=post_data.body,
+    #                     hyperlink=post_data.hyperlink,
+    #                     author_id=post_data.author_id,
+    #                     created_at=created_at,
+    #                     )
+    #                 return None
+    #     except Exception as e:
+    #         print(e)
+    #         return None
--- api/routers/posts.py
+++ api/routers/posts.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
-from queries.posts import PostQueries, PostOut
+from queries.posts import PostQueries, PostOut, PostIn
 from typing import List
 from authenticator import authenticator
-
+from .users import UserToken
 router = APIRouter()
 
 
@@ -17,6 +17,75 @@ async def get_all_posts(
     try:
         return repo.get_all()
     except Exception:
+        raise HTTPException(status_code=400, detail="Could not retrieve posts.")
+
+
+@router.post(
+    "/api/posts",
+    response_model=PostOut,
+)
+async def create_post(
+    post_data: PostIn,
+    repo: PostQueries = Depends(),
+    user: UserToken = Depends(authenticator.get_current_account_data),
+):
+    try:
+        new_post = repo.create_post(post_data, user["id"])
+        return new_post
+    except Exception:
+        raise HTTPException(status_code=400, detail="could not create post.")
+
+
+@router.delete("/api/posts/{post_id}", response_model=bool)
+async def delete_post(
+    post_id: int,
+    repo: PostQueries = Depends(),
+    user: UserToken = Depends(authenticator.get_current_account_data),
+):
+    try:
+        deleted = repo.delete_post(post_id, user["id"])
+        if deleted:
+            return True
+        else:
+            raise HTTPException(status_code=404, detail="Post not found")
+    except Exception:
+        raise HTTPException(status_code=400, detail="You are not the author of this post.")
+
+
+@router.get(
+    "/api/posts/{post_id}",
+    response_model=PostOut,
+)
+async def get_post_by_id(
+    post_id: int,
+    repo: PostQueries = Depends(),
+):
+    try:
+        post = repo.get_post_by_id(post_id)
+        if not post:
+            raise HTTPException(status_code=404, detail="Post not found")
+
+        return post
+    except Exception:
         raise HTTPException(
-            status_code=400, detail="Could not retrieve posts."
+            status_code=500,
+            detail="An error occurred while fetching the post.",
         )
+# # put request to update specific post and still working
+# @router.put(
+#     "/api/posts/{post_id}",
+#     response_model=PostOut,
+# )
+# async def update_post(
+#     post_id: int,
+#     post_data: PostIn,
+#     repo: PostQueries = Depends(),
+#     account_data: dict = Depends(authenticator.get_current_account_data),
+# ):
+#     try:
+#         updated_post = repo.update_post(post_id, post_data)
+#         if not updated_post:
+#             raise HTTPException(status_code=404, detail="Post not found")
+#         return updated_post
+#     except Exception:
+#         raise HTTPException(status_code=500, detail="An error occurred while updating the post.")
