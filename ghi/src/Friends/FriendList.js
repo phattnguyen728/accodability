4a78008d5ae02e974a44f9cbf3a001e61ea96832
Hung <hungduyhoang@gmail.com>
2023-09-08 16:30:18 -0700

    fixed the eslint UseEffect errors as well as refactored approve button code. Updated REACT_APP_API_HOST to https://accodabilityapi.mod3projects.com . Added back the ESlint into package.json

--- ghi/src/Friends/FriendList.js
+++ ghi/src/Friends/FriendList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useAuthContext, useToken } from "@galvanize-inc/jwtdown-for-react";
+import { useAuthContext } from "@galvanize-inc/jwtdown-for-react";
 import jwtDecode from "jwt-decode";
 
 function FriendList() {
@@ -7,41 +7,6 @@ function FriendList() {
   const [friends, setFriends] = useState([]);
   const [userId, setUserId] = useState(null);
   const [userProfiles, setUserProfiles] = useState({});
-
-  // const [search, setSearch] = useState("");
-  // const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    if (token) {
-      try {
-        const decodedToken = jwtDecode(token);
-        const userId = parseInt(decodedToken.sub, 10);
-        setUserId(userId);
-      } catch (error) {
-        console.error("Error decoding token:", error.message);
-      }
-    }
-  }, [token]);
-
-  const fetchFriends = async () => {
-    const fetchConfig = {
-      method: "get",
-      headers: {
-        Authentication: `Bearer ${token}`,
-      },
-      credentials: "include",
-    };
-    const url = `${process.env.REACT_APP_API_HOST}/friends`;
-    const response = await fetch(url, fetchConfig);
-    console.log(response);
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-      setFriends(data);
-    } else {
-      console.error(response);
-    }
-  };
 
   const handleApprove = async (sender_id, friendRequestId) => {
     const updateFriend = {
@@ -58,46 +23,91 @@ function FriendList() {
       credentials: "include",
       body: JSON.stringify(updateFriend),
     };
-    const response = await fetch(acceptUrl, fetchConfig).then(() => {
-      fetchFriends();
-      // console.log(response);
-    });
-  };
 
-  const fetchUserProfile = async (senderId) => {
-    const fetchConfig = {
-      method: "get",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      credentials: "include",
-    };
-    const url = `${process.env.REACT_APP_API_HOST}/users/${senderId}`;
-
-    const response = await fetch(url, fetchConfig);
-
+    const response = await fetch(acceptUrl, fetchConfig);
     if (response.ok) {
-      const userProfile = await response.json();
-      setUserProfiles((currentProfiles) => {
-        const updatedProfiles = { ...currentProfiles };
-        updatedProfiles[senderId] = userProfile;
-        return updatedProfiles;
-      });
-    } else {
-      console.error(response);
+      const fetchConfig = {
+        method: "get",
+        headers: {
+          Authentication: `Bearer ${token}`,
+        },
+        credentials: "include",
+      };
+      const url = `${process.env.REACT_APP_API_HOST}/friends`;
+      const response = await fetch(url, fetchConfig);
+
+      if (response.ok) {
+        const data = await response.json();
+
+        setFriends(data);
+      } else {
+        console.error(response);
+      }
     }
   };
+  useEffect(() => {
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+        const userId = parseInt(decodedToken.sub, 10);
+        setUserId(userId);
+      } catch (error) {
+        console.error("Error decoding token:", error.message);
+      }
+    }
+  }, [token, friends]);
 
   useEffect(() => {
+    const fetchFriends = async () => {
+      const fetchConfig = {
+        method: "get",
+        headers: {
+          Authentication: `Bearer ${token}`,
+        },
+        credentials: "include",
+      };
+      const url = `${process.env.REACT_APP_API_HOST}/friends`;
+      const response = await fetch(url, fetchConfig);
+      if (response.ok) {
+        const data = await response.json();
+        setFriends(data);
+      } else {
+        console.error(response);
+      }
+    };
+
     fetchFriends();
   }, [token]);
 
   useEffect(() => {
+    const fetchUserProfile = async (senderId) => {
+      const fetchConfig = {
+        method: "get",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        credentials: "include",
+      };
+      const url = `${process.env.REACT_APP_API_HOST}/users/${senderId}`;
+
+      const response = await fetch(url, fetchConfig);
+
+      if (response.ok) {
+        const userProfile = await response.json();
+        setUserProfiles((currentProfiles) => {
+          const updatedProfiles = { ...currentProfiles };
+          updatedProfiles[senderId] = userProfile;
+          return updatedProfiles;
+        });
+      } else {
+        console.error(response);
+      }
+    };
     friends.forEach((friend) => {
       const senderId = friend.sender_id;
       fetchUserProfile(senderId);
     });
-  }, [friends]);
+  }, [friends, token]);
 
   return (
     <div>
