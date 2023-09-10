2e65c3e2e14ca858dec756b46044b301b8875d4c
Hung Hoang <hungduyhoang@gmail.com>
2023-09-10 23:32:21 +0000

    added our deployment url to CORS allowed origins on main.py

--- api/main.py
+++ api/main.py
@@ -7,15 +7,29 @@ import os
 
 
 app = FastAPI()
+
+
 app.include_router(authenticator.router)
 app.include_router(users.router)
 app.include_router(friends.router)
 app.include_router(posts.router)
 app.include_router(comments.router)
 app.include_router(messages.router)
+
+origins = [
+    os.environ.get("CORS_HOST", "http://localhost:3000"),
+    "https://its-not-a-phad.gitlab.io",
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:3000")],
+    # allow_origins=[
+    #     os.environ.get(
+    #         "CORS_HOST",
+    #         "http://localhost:3000",
+    #         "https://its-not-a-phad.gitlab.io",
+    #     )
+    # ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
